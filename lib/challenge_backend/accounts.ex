8004e78a9f8cb250a3d6fdfce3cd35bfe9acd853
Jonatan da Hora <jonatandahora@gmail.com>
2023-11-08 00:14:09 -0300

    fix: remove unnecessary functions

--- lib/challenge_backend/accounts.ex
+++ lib/challenge_backend/accounts.ex
@@ -9,19 +9,6 @@ defmodule ChallengeBackend.Accounts do
   alias ChallengeBackend.Accounts.UserAccount
 
   @doc """
-  Returns the list of user_accounts.
-
-  ## Examples
-
-      iex> list_user_accounts()
-      [%UserAccount{}, ...]
-
-  """
-  def list_user_accounts do
-    Repo.all(UserAccount)
-  end
-
-  @doc """
   Gets a single user_account.
 
 
@@ -36,6 +23,28 @@ defmodule ChallengeBackend.Accounts do
   """
   def get_user_account(id) do
     case Repo.get(UserAccount, id) do
+      nil -> {:error, :not_found}
+      user_account -> {:ok, user_account}
+    end
+  end
+
+  @doc """
+  Fetchs a single user_account to be used in a transaction and locks it to prevent concurrency errors.
+
+
+  ## Examples
+
+      iex> fetch_lock_user_account(123)
+      {:ok, %UserAccount{}}
+
+      iex> fetch_lock_user_account!(456)
+      {:error, :not_found}
+
+  """
+  def fetch_lock_user_account(id) do
+    query = from(uc in UserAccount, where: uc.id == ^id, lock: "FOR UPDATE")
+
+    case Repo.one(query) do
       nil -> {:error, :not_found}
       user_account -> {:ok, user_account}
     end
@@ -59,55 +68,9 @@ defmodule ChallengeBackend.Accounts do
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a user_account.
-
-  ## Examples
-
-      iex> update_user_account(user_account, %{field: new_value})
-      {:ok, %UserAccount{}}
-
-      iex> update_user_account(user_account, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_user_account(%UserAccount{} = user_account, attrs) do
-    user_account
-    |> UserAccount.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a user_account.
-
-  ## Examples
-
-      iex> delete_user_account(user_account)
-      {:ok, %UserAccount{}}
-
-      iex> delete_user_account(user_account)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_user_account(%UserAccount{} = user_account) do
-    Repo.delete(user_account)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking user_account changes.
-
-  ## Examples
-
-      iex> change_user_account(user_account)
-      %Ecto.Changeset{data: %UserAccount{}}
-
-  """
-  def change_user_account(%UserAccount{} = user_account, attrs \\ %{}) do
-    UserAccount.changeset(user_account, attrs)
-  end
-
   def add_balance(%UserAccount{} = user_account, %Decimal{} = amount) do
     new_amount = Decimal.add(user_account.balance, amount)
+
     user_account
     |> UserAccount.update_balance_changeset(new_amount)
     |> Repo.update()
@@ -115,6 +78,7 @@ defmodule ChallengeBackend.Accounts do
 
   def subtract_balance(%UserAccount{} = user_account, %Decimal{} = amount) do
     new_amount = Decimal.sub(user_account.balance, amount)
+
     user_account
     |> UserAccount.update_balance_changeset(new_amount)
     |> Repo.update()
--- test/challenge_backend/accounts_test.exs
+++ test/challenge_backend/accounts_test.exs
@@ -10,14 +10,14 @@ defmodule ChallengeBackend.AccountsTest do
 
     @invalid_attrs %{first_name: nil, last_name: nil, cpf: nil, password: nil, balance: nil}
 
-    test "list_user_accounts/0 returns all user_accounts" do
-      user_account = user_account_fixture()
-      assert Accounts.list_user_accounts() == [user_account]
-    end
-
     test "get_user_account/1 returns the user_account with given id" do
       user_account = user_account_fixture()
       assert Accounts.get_user_account(user_account.id) == {:ok, user_account}
+    end
+
+    test "fetch_lock_user_account/1 returns the user_account with given id" do
+      user_account = user_account_fixture()
+      assert Accounts.fetch_lock_user_account(user_account.id) == {:ok, user_account}
     end
 
     test "create_user_account/1 with valid data creates a user_account" do
@@ -63,47 +63,6 @@ defmodule ChallengeBackend.AccountsTest do
       invalid_attrs = Map.put(@invalid_attrs, :cpf, "123456")
       assert {:error, %Ecto.Changeset{} = changeset} = Accounts.create_user_account(invalid_attrs)
       assert errors_on(changeset)[:cpf]
-    end
-
-    test "update_user_account/2 with valid data updates the user_account" do
-      user_account = user_account_fixture()
-
-      update_attrs = %{
-        first_name: "some updated first_name",
-        last_name: "some updated last_name",
-        cpf: "21946843601",
-        password: "some updated password",
-        balance: "4567"
-      }
-
-      assert {:ok, %UserAccount{} = user_account} =
-               Accounts.update_user_account(user_account, update_attrs)
-
-      assert user_account.first_name == "some updated first_name"
-      assert user_account.last_name == "some updated last_name"
-      assert user_account.cpf == "21946843601"
-      assert Argon2.verify_pass("some updated password", user_account.password_hash)
-      assert user_account.balance == Decimal.new("4567")
-    end
-
-    test "update_user_account/2 with invalid data returns error changeset" do
-      user_account = user_account_fixture()
-
-      assert {:error, %Ecto.Changeset{}} =
-               Accounts.update_user_account(user_account, @invalid_attrs)
-
-      assert {:ok, user_account} == Accounts.get_user_account(user_account.id)
-    end
-
-    test "delete_user_account/1 deletes the user_account" do
-      user_account = user_account_fixture()
-      assert {:ok, %UserAccount{}} = Accounts.delete_user_account(user_account)
-      assert {:error, :not_found} = Accounts.get_user_account(user_account.id)
-    end
-
-    test "change_user_account/1 returns a user_account changeset" do
-      user_account = user_account_fixture()
-      assert %Ecto.Changeset{} = Accounts.change_user_account(user_account)
     end
   end
 end
