587b6b13ce40cbf13c087f35b4ee1d35e8c3a4af
Jonatan da Hora <jonatandahora@gmail.com>
2023-11-08 21:50:44 -0300

    fix: moved account test file

--- test/challenge_backend_web/controllers/accounts_controller_test.exs
+++ test/challenge_backend_web/controllers/accounts_controller_test.exs
@@ -25,40 +25,40 @@ defmodule ChallengeBackendWeb.AccountsControllerTest do
 
   describe "sign up" do
     test "renders user_account when data is valid", %{conn: conn} do
-      conn = post(conn, ~p"/api/sign_up", @sign_up_attrs)
+      conn = post(conn, ~p"/api/accounts", @sign_up_attrs)
       assert json_response(conn, 201)["data"]
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, ~p"/api/sign_up", @invalid_sign_up_attrs)
+      conn = post(conn, ~p"/api/accounts", @invalid_sign_up_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
 
   describe "login" do
-    test "login successfull with valid credentials", %{conn: conn} do
+    test "successfull with valid credentials", %{conn: conn} do
       user_account = %{id: user_account_id} = user_account_fixture(%{password: "123456"})
 
-      conn = post(conn, ~p"/api/login", %{cpf: user_account.cpf, password: "123456"})
+      conn = post(conn, ~p"/api/accounts/login", %{cpf: user_account.cpf, password: "123456"})
       assert token = json_response(conn, 200)["data"]["token"]
 
       assert {:ok, %{"sub" => ^user_account_id}} = decode_and_verify(token)
     end
 
-    test "login failed with invalid password", %{conn: conn} do
+    test "failed with invalid password", %{conn: conn} do
       user_account = user_account_fixture(%{password: "123456"})
 
-      conn = post(conn, ~p"/api/login", %{cpf: user_account.cpf, password: "654321"})
+      conn = post(conn, ~p"/api/accounts/login", %{cpf: user_account.cpf, password: "654321"})
 
       assert %{"errors" => %{"detail" => "Unauthorized"}} == json_response(conn, 401)
     end
 
-    test "login failed with invalid cpf", %{conn: conn} do
+    test "failed with invalid cpf", %{conn: conn} do
       user_account_fixture(%{password: "123456"})
 
-      conn = post(conn, ~p"/api/login", %{cpf: Brcpfcnpj.cpf_generate(), password: "123456"})
+      conn = post(conn, ~p"/api/accounts/login", %{cpf: Brcpfcnpj.cpf_generate(), password: "123456"})
 
-      assert %{"errors" => %{"detail" => "Unauthorized"}} == json_response(conn, 401)
+      assert %{"errors" => %{"detail" => "Not Found"}} == json_response(conn, 404)
     end
   end
 
@@ -70,13 +70,13 @@ defmodule ChallengeBackendWeb.AccountsControllerTest do
       conn =
         conn
         |> put_req_header("authorization", "Bearer " <> token)
-        |> get(~p"/api/balance")
+        |> get(~p"/api/accounts/balance")
 
       assert json_response(conn, 200)["data"]["balance"] == "1250"
     end
 
     test "renders error when account token is not added to header", %{conn: conn} do
-      conn = get(conn, ~p"/api/balance")
+      conn = get(conn, ~p"/api/accounts/balance")
 
       assert %{"error" => "unauthenticated"} == json_response(conn, 401)
     end
